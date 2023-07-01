9ed757b4a9e88b90f160d0d51e3234af8271ae32
Artem Popov <br4v0.2403@gmail.com>
2023-07-01 17:01:56 +0300

    set up auth classes

---
+++ src/main/java/com/example/springsecurity_jwt/auth/AuthenticationService.java
@@ -0,0 +1,53 @@
+package com.example.springsecurity_jwt.auth;
+
+import com.example.springsecurity_jwt.config.JwtService;
+import com.example.springsecurity_jwt.user.Role;
+import com.example.springsecurity_jwt.user.User;
+import com.example.springsecurity_jwt.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+    private final UserRepository repository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticationResponse register(RegisterRequest request) {
+        var user = User.builder()
+                .name(request.getName())
+                .lastName(request.getLastName())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+        repository.save(user);
+
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+
+        var user = repository.findByEmail(request.getEmail())
+                .orElseThrow();
+
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+}
---
+++ src/main/java/com/example/springsecurity_jwt/auth/RegisterRequest.java
@@ -0,0 +1,18 @@
+package com.example.springsecurity_jwt.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterRequest {
+
+    private String name;
+    private String lastName;
+    private String email;
+    private String password;
+}
